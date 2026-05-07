d44215f1e1326b0ec1b9ed5ccbf922fcd53756f0
zmsistemas2025-bit <zmsistemas2025@gmail.com>
2026-05-07 11:59:07 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
 
 const PORT = process.env.PORT || 3000
 
+app.use(express.static(__dirname))
+
 app.get('/', (req, res) => {
-  res.send('ZM CRM funcionando 🚀')
+  res.sendFile(path.join(__dirname, 'index.html'))
 })
 
 app.listen(PORT, () => {
